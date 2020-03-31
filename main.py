7b8fe3cdd176c0c883ccf6740e7519c837ca86e7
Matt Williams <matt@milliams.com>
2020-03-31 18:06:15 +0100

    Pass os.environ to the subprocess call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import os
 import subprocess
 from pathlib import Path
 
@@ -16,7 +17,7 @@ extra_args = " ".join(f"{k}={v}" for k, v in app_meta["variables"].items())
 
 r = subprocess.run(
     ["ansible-playbook", "--extra-vars", extra_args, app_dir / "install.yaml"],
-    env={"ANSIBLE_ROLES_PATH": "roles:~/.ansible/roles"},
+    env=dict({"ANSIBLE_ROLES_PATH": "roles:~/.ansible/roles"}, **os.environ),
     #stdout=subprocess.PIPE,
     #stderr=subprocess.PIPE,
 )
