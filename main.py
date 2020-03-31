012e744b398bfb772bde5810af286a9d889c46aa
Matt Williams <matt@milliams.com>
2020-03-31 18:03:41 +0100

    Fix role path and requirements.yml syntax

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ extra_args = " ".join(f"{k}={v}" for k, v in app_meta["variables"].items())
 
 r = subprocess.run(
     ["ansible-playbook", "--extra-vars", extra_args, app_dir / "install.yaml"],
-    env={"ANSIBLE_ROLES_PATH": "/home/matt/projects/citc-apps/roles"},
+    env={"ANSIBLE_ROLES_PATH": "roles:~/.ansible/roles"},
     #stdout=subprocess.PIPE,
     #stderr=subprocess.PIPE,
 )
