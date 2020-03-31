86d5b8923ce2c626fc190d5a1580b82516df6bb6
Matt Williams <matt@milliams.com>
2020-03-31 17:39:01 +0100

    Start work on installer

---
+++ main.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import subprocess
+from pathlib import Path
+
+import yaml
+
+app = "jupyterhub"
+
+app_dir = Path(app)
+
+with open(app_dir / "meta.yaml") as f:
+    app_meta = yaml.safe_load(f)
+
+extra_args = " ".join(f"{k}={v}" for k, v in app_meta["variables"].items())
+
+r = subprocess.run(
+    ["ansible-playbook", "--extra-vars", extra_args, app_dir / "install.yaml"],
+    env={"ANSIBLE_ROLES_PATH": "/home/matt/projects/citc-apps/roles"},
+    #stdout=subprocess.PIPE,
+    #stderr=subprocess.PIPE,
+)
+
+#print(r.stdout.decode())
